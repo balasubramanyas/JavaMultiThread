adaf59736301204818beba7333e93ec73a666180
Balasubramanya <32163715+balasubramanyas@users.noreply.github.com>
2019-03-10 21:52:32 +0530

    Create Project

---
+++ src/com/balasubramanya/thread/simplethread/RunnableSimpleThread.java
@@ -0,0 +1,10 @@
+package com.balasubramanya.thread.simplethread;
+
+public class RunnableSimpleThread implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("This Thread Implemented Using Runnable Interface");
+	}
+
+}
